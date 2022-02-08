91b6e0e122f3de003c78dc03969f49376d267dec
Maximilian <maxmueller2003@web.de>
2022-02-08 21:22:51 +0100

    Urne.java now creates 49 Kugeln, also ziehen() return one Kugel (lottery ball)

--- src/Urne.java
+++ src/Urne.java
@@ -1,3 +1,22 @@
 public class Urne {
-    
+    private Kugel[] dieKugeln;
+    private int anzahl;
+
+    public Urne() {
+        dieKugeln = new Kugel[49];
+        anzahl = 49;
+        for (int i = 0; i < anzahl; i++) {
+            dieKugeln[i] = new Kugel(i+1);
+        }
+    }
+
+    public Kugel ziehen() {
+        int position = (int) (anzahl * Math.random());
+        Kugel eineKugel = dieKugeln[position];
+        for (int i = position; i < anzahl; i++) {
+            dieKugeln[i] = dieKugeln[i +1];
+        }
+        anzahl--;
+        return eineKugel;
+    }
 }
