c4b6ce65d31af11bd04dedd6a4a1def663ab4b8a
Maximilian <maxmueller2003@web.de>
2022-02-13 16:58:49 +0100

    optimized sorting algorithm

--- src/Ziehung.java
+++ src/Ziehung.java
@@ -7,26 +7,59 @@ public class Ziehung {
         dieUrne = new Urne();
         lottozahlen = new int[6];
         ziehen();
-        sortieren();
+        sortieren(lottozahlen);
     }
 
-    private void sortieren() {
-        for (int i = 0; i < lottozahlen.length; i++) {
-            if (i + 1 < lottozahlen.length) {
-                if (lottozahlen[i] > lottozahlen[i + 1]) {
-                    int temp = lottozahlen[i];
-                    lottozahlen[i] = lottozahlen[i + 1];
-                    lottozahlen[i + 1] = temp;
-                }
-            } else {
-                if (lottozahlen[0] > lottozahlen[1]) {
-                    int temp = lottozahlen[0];
-                    lottozahlen[0] = lottozahlen[1];
-                    lottozahlen[1] = temp;
-                    i = 0;
-                }
-            }
+    private void sortieren(int[] inputArray) {
+        int arrayLength = inputArray.length;
+    
+        if (inputArray.length < 2) {
+            return;
+        }
+    
+        int midIndex = inputArray.length / 2;
+        int[] leftArray = new int[midIndex];
+        int[] rightArray = new int[arrayLength - midIndex];
+    
+        for (int i = 0; i < midIndex; i++) {
+            leftArray[i] = inputArray[i]; //fill the leftHalf Array with the first numbers from the input Array
+            
+        }
+        for (int i = midIndex; i < arrayLength; i++) {
+            rightArray[i - midIndex] = inputArray[i]; //fill the rightHalf Array with the last numbers from the input Array
+        }
 
+        sortieren(leftArray);
+        sortieren(rightArray);
+    
+        merge(inputArray, leftArray, rightArray);
+    }
+    
+    private void merge(int[] inputArray, int[] leftArray, int[] rightArray) {
+        int leftArraySize = leftArray.length;
+        int rightArraySize = rightArray.length;
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftArraySize && j < rightArraySize) {
+            if (leftArray[i] <= rightArray[j]) {
+                inputArray[k] = leftArray[i];
+                i++;
+            } else {
+                inputArray[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftArraySize) {
+            inputArray[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < rightArraySize) {
+            inputArray[k] = rightArray[j];
+            j++;
+            k++;
         }
     }
 
@@ -38,7 +71,7 @@ public class Ziehung {
     }
 
     public int getLottozahl(int nr) {
-        return lottozahlen[nr - 1];
+        return lottozahlen[nr];
     }
 
     public int getZusatzzahl() {
